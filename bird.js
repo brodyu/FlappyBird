87e034359519930be094f128dab778afb6627eef
Brody <brodyuehara@gmail.com>
2020-04-23 15:48:48 -1000

    Added files from dev

---
+++ bird.js
@@ -0,0 +1,31 @@
+function Bird() {
+  this.y = height/2;
+  this.x = 64;
+
+  this.gravity = 0.6;
+  this.lift = -15;
+  this.velocity = 0;
+
+  this.show = function() {
+    fill(255);
+    ellipse(this.x, this.y, 32, 32);
+  }
+
+  this.up = function() {
+    this.velocity += this.lift;
+  }
+
+  this.update = function() {
+    this.velocity += this.gravity;
+    this.velocity *= 0.95;
+    this.y += this.velocity;
+
+    if (this.y > height) {
+      this.y = height;
+      this.velocty = 0;
+    } else if (this.y < 0) {
+      this.y = 0;
+      this.velocity = 0;
+    }
+  }
+}
