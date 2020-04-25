3e9df608a1b51d59e2d285fe9e40fb752cda713a
Brody <brodyuehara@gmail.com>
2020-04-24 15:55:56 -1000

    BaseCode in OOP

--- bird.js
+++ bird.js
@@ -3,64 +3,33 @@ class Bird {
     this.y = height / 2;
     this.x = 64;
 
-    this.gravity = 0.6;
-    this.lift = -13;
+    this.gravity = 0.7;
+    this.lift = -12;
     this.velocity = 0;
-
-    this.icon = birdSprite;
-    this.width = 64;
-    this.height = 64;
-
-    this.brain = new NeuralNetwork(4, 4, 1);
   }
 
   show() {
-    // draw the icon CENTERED around the X and Y coords of the bird object
-    image(this.icon, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
-  }
+    fill(255);
+    ellipse(this.x, this.y, 32, 32);
+  };
 
   up() {
-    this.velocity = this.lift;
-  }
-
-  think(pipes) {
-
-    //find the closest pipe
-    let closest = null;
-    let closestD = Infinity;
-    for (let i = 0; i < pipes.length; i++) {
-      let d = pipes[i].x - this.x;
-      if (d < closestD && d > 0) {
-        closest = pipes[i];
-        closestD = d;
-      }
-    }
-
-    //AI thinks about what to do
-    let inputs = [];
-    inputs[0] = this.y / height;
-    inputs[1] = closest.top / height;
-    inputs[2] = closest.bottom / height;
-    inputs[3] = closest.x / width;
-
-    let output = this.brain.predict(inputs);
-    if (output[0] > 0.5) {
-      this.up();
-    }
-  }
+    this.velocity += this.lift;
+  };
 
   update() {
     this.velocity += this.gravity;
+    // this.velocity *= 0.9;
     this.y += this.velocity;
 
-    if (this.y >= height - this.height / 2) {
-      this.y = height - this.height / 2;
+    if (this.y > height) {
+      this.y = height;
       this.velocity = 0;
     }
 
-    if (this.y <= this.height / 2) {
-      this.y = this.height / 2;
+    if (this.y < 0) {
+      this.y = 0;
       this.velocity = 0;
     }
-  }
+  };
 }
