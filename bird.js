e2ca31c5b02d5fe4c6b8d8d4269815eec4d4e909
Brody <brodyuehara@gmail.com>
2020-04-24 22:57:14 -1000

    Finished neuroevolution

--- bird.js
+++ bird.js
@@ -3,18 +3,16 @@ class Bird {
     this.y = height / 2;
     this.x = 64;
 
-    this.gravity = 0.7;
+    this.gravity = 0.8;
     this.lift = -12;
     this.velocity = 0;
-    // Criteria score for next generation
+
     this.score = 0;
     this.fitness = 0;
-
     if (brain) {
       this.brain = brain.copy();
     } else {
-      // Brain of the bird with four inputs, four hidden layers, and one output
-      this.brain = new NeuralNetwork(4, 4, 2);
+      this.brain = new NeuralNetwork(5, 8, 2);
     }
   }
 
@@ -22,21 +20,22 @@ class Bird {
     stroke(255);
     fill(255, 100);
     ellipse(this.x, this.y, 32, 32);
-  };
+  }
 
   up() {
     this.velocity += this.lift;
-  };
+  }
 
   mutate() {
     this.brain.mutate(0.1);
   }
 
   think(pipes) {
+    // Find the closest pipe
     let closest = null;
     let closestD = Infinity;
     for (let i = 0; i < pipes.length; i++) {
-      let d = pipes[i].x - this.x;
+      let d = pipes[i].x + pipes[i].w - this.x;
       if (d < closestD && d > 0) {
         closest = pipes[i];
         closestD = d;
@@ -48,27 +47,23 @@ class Bird {
     inputs[1] = closest.top / height;
     inputs[2] = closest.bottom / height;
     inputs[3] = closest.x / width;
-
+    inputs[4] = this.velocity / 10;
     let output = this.brain.predict(inputs);
+    //if (output[0] > output[1] && this.velocity >= 0) {
     if (output[0] > output[1]) {
       this.up();
     }
   }
 
+  offScreen() {
+    return this.y > height || this.y < 0;
+  }
+
   update() {
     this.score++;
+
     this.velocity += this.gravity;
-    // this.velocity *= 0.9;
+    //this.velocity *= 0.9;
     this.y += this.velocity;
-
-    if (this.y > height) {
-      this.y = height;
-      this.velocity = 0;
-    }
-
-    if (this.y < 0) {
-      this.y = 0;
-      this.velocity = 0;
-    }
-  };
+  }
 }
--- ga.js
+++ ga.js
@@ -1,7 +1,6 @@
 function nextGeneration() {
-
+  console.log('next generation');
   calculateFitness();
-
   for (let i = 0; i < TOTAL; i++) {
     birds[i] = pickOne();
   }
@@ -9,7 +8,14 @@ function nextGeneration() {
 }
 
 function pickOne() {
-  let bird = random(savedBirds);
+  let index = 0;
+  let r = random(1);
+  while (r > 0) {
+    r = r - savedBirds[index].fitness;
+    index++;
+  }
+  index--;
+  let bird = savedBirds[index];
   let child = new Bird(bird.brain);
   child.mutate();
   return child;
@@ -17,10 +23,10 @@ function pickOne() {
 
 function calculateFitness() {
   let sum = 0;
-  for (let bird of birds) {
+  for (let bird of savedBirds) {
     sum += bird.score;
   }
-  for (let bird of birds) {
+  for (let bird of savedBirds) {
     bird.fitness = bird.score / sum;
   }
 }
