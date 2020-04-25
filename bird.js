55900f0149cb955bd1f137afa0288f4b98b5f43d
Brody <brodyuehara@gmail.com>
2020-04-24 17:53:22 -1000

    Edits to NeuroEvolutionary Algo

--- bird.js
+++ bird.js
@@ -1,15 +1,26 @@
 class Bird {
-  constructor() {
+  constructor(brain) {
     this.y = height / 2;
     this.x = 64;
 
     this.gravity = 0.7;
     this.lift = -12;
     this.velocity = 0;
+    // Criteria score for next generation
+    this.score = 0;
+    this.fitness = 0;
+
+    if (brain) {
+      this.brain = brain.copy();
+    } else {
+      // Brain of the bird with four inputs, four hidden layers, and one output
+      this.brain = new NeuralNetwork(4, 4, 2);
+    }
   }
 
   show() {
-    fill(255);
+    stroke(255);
+    fill(255, 100);
     ellipse(this.x, this.y, 32, 32);
   };
 
@@ -17,7 +28,35 @@ class Bird {
     this.velocity += this.lift;
   };
 
+  mutate() {
+    this.brain.mutate(0.1);
+  }
+
+  think(pipes) {
+    let closest = null;
+    let closestD = Infinity;
+    for (let i = 0; i < pipes.length; i++) {
+      let d = pipes[i].x - this.x;
+      if (d < closestD && d > 0) {
+        closest = pipes[i];
+        closestD = d;
+      }
+    }
+
+    let inputs = [];
+    inputs[0] = this.y / height;
+    inputs[1] = closest.top / height;
+    inputs[2] = closest.bottom / height;
+    inputs[3] = closest.x / width;
+
+    let output = this.brain.predict(inputs);
+    if (output[0] > output[1]) {
+      this.up();
+    }
+  }
+
   update() {
+    this.score++;
     this.velocity += this.gravity;
     // this.velocity *= 0.9;
     this.y += this.velocity;
--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
-var bird;
-var pipes = [];
+const TOTAL = 250;
+let birds = [];
+let savedBirds = [];
+let pipes = [];
 
 function setup() {
   createCanvas(640, 480);
-  bird = new Bird();
+  for (let i = 0; i < TOTAL; i++) {
+      birds[i] = new Bird();
+  }
   pipes.push(new Pipe());
 }
 
@@ -14,26 +18,39 @@ function draw() {
     pipes[i].show();
     pipes[i].update();
 
-    if (pipes[i].hits(bird)) {
-      console.log('HIT');
+    for (let j = birds.length-1; j >=0; j--) {
+      if (pipes[i].hits(birds[j])) {
+        savedBirds.push(birds.splice(j, 1)[0]);
+      }
     }
+
+    // if (pipes[i].hits(bird)) {
+    //   console.log('HIT');
+    // }
 
     if (pipes[i].offscreen()) {
       pipes.splice(i, 1);
     }
   }
 
-  bird.update();
-  bird.show();
+  for (let bird of birds) {
+    bird.think(pipes);
+    bird.update();
+    bird.show();
+  }
+
+  if (birds.length === 0) {
+    nextGeneration();
+  }
 
   if (frameCount % 75 == 0) {
     pipes.push(new Pipe());
   }
 }
 
-function keyPressed() {
-  if (key == ' ') {
-    bird.up();
-    //console.log("SPACE");
-  }
-}
+// function keyPressed() {
+//   if (key == ' ') {
+//     bird.up();
+//     //console.log("SPACE");
+//   }
+// }
